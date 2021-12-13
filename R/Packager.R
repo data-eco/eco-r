e0cf219db40ce18e366403832ab41fbdb7702db3
Keith Hughitt <keith.hughitt@gmail.com>
2021-12-12 21:06:05 -0700

    added "update_package" method to build off of an existing datapackage.

--- R/Packager.R
+++ R/Packager.R
@@ -1,6 +1,6 @@
 #' An R6 class for assisting with the packaging of "Io" x "DataDAG" formatted data packages
 #'
-#' @importFrom frictionless add_resource create_package
+#' @importFrom frictionless add_resource create_package read_package
 #' @importFrom glue glue
 #' @importFrom R6 R6Class
 #' @importFrom uuid UUIDgenerate
@@ -18,16 +18,12 @@ Packager <- R6Class("Packager",
     #' @description
     #' Creates a new "Packager" instance.
     #'
-    #' @param recipe Io x DataDAG recipe
     #' @return A new `Packager` object.
-    initialize = function(recipe) {
+    initialize = function() {
       private$conf_dir <- file.path(getwd(), "conf")
       private$schema_dir <- file.path(getwd(), "schema")
 
       private$config <- private$load_io_config()
-
-      # load & validate recipe
-      self$recipe <- private$load_recipe(recipe)
 
       # TODO: load data model config files
       # self.analyses = self._load_config("analyses")
@@ -38,13 +34,17 @@ Packager <- R6Class("Packager",
     #' @description
     #' Builds Io/DataDAG data package.
     #'
+    #' @param recipe Io x DataDAG recipe
     #' @param data data.frame Main dataset
     #' @param row_metadata data.frame (Optional) row metadata
     #' @param col_metadata data.frame (Optional) col metadata
-    build_package = function(data, row_metadata=NULL, col_metadata=NULL) {
+    build_package = function(recipe, data, row_metadata=NULL, col_metadata=NULL) {
+      # load & validate recipe
+      self$recipe <- private$load_recipe(recipe)
+
       # create package and add resources
       pkg <- create_package() %>%
-        add_resource("data", expr)
+        add_resource("data", data)
 
       pkg$resources[[1]]$type <- "dataset"
 
@@ -67,21 +67,92 @@ Packager <- R6Class("Packager",
       # create custom "io" metadata section
       mdata <- self$recipe
 
+      # generate dataset uuid and add to metadata
+      mdata$data$uuid <- UUIDgenerate()
+
+      # convert recipe provenance section to a dag
       mdata$provenance <- list(
-        "nodes" = list(
-          "root" = mdata$provenance,
-          "io-packager" = list(
-            "time" = now,
-            "version" = private$version
-          )
-        ),
-        "edges" = list(
-          c("root", "io-packager")
-        )
+        "nodes" = setNames(list(mdata$provenance), mdata$data$uuid),
+        "edges" = list()
+      )
+
+      # add packager-related details
+      mdata$provenance$nodes[[mdata$data$uuid]]$io <- list(
+        "name" = "io-r",
+        "time" = now,
+        "version" = private$version
       )
 
       mdata$contributors <- private$config$metadata$contributors
-      mdata$uuid <- UUIDgenerate()
+
+      # add io metadata section
+      pkg$io <- mdata
+
+      return(pkg)
+    },
+
+    #' @description
+    #' Builds a new Io/DataDAG data package, using an old one
+    #'
+    #' @param path Path to original datapackage
+    #' @param updates List with changes to be made to the metadata
+    #' @param action Name of action to use in provenance entry
+    #' @param data data.frame Main dataset
+    #' @param row_metadata data.frame (Optional) row metadata
+    #' @param col_metadata data.frame (Optional) col metadata
+    update_package = function(path, updates, action, data, row_metadata=NULL, col_metadata=NULL) {
+      # extract io block from previous datapackage, and apply any updates
+      if (!file.exists(path)) {
+        stop(glue("[Error] No datapackage found at {path}!"))
+      }
+
+      prev_pkg <- read_package(path)
+
+      self$recipe <- prev_pkg$io
+      self$recipe <- modifyList(self$recipe, updates)
+
+      # create new data package and add resources
+      pkg <- create_package() %>%
+        add_resource("data", data)
+
+      pkg$resources[[1]]$type <- "dataset"
+
+      if (!is.null(row_metadata)) {
+        pkg <- pkg %>%
+          add_resource("row-metadata", row_metadata)
+
+        pkg$resources[[length(pkg$resources)]]$type <- "row-metadata"
+      }
+      if (!is.null(col_metadata)) {
+        pkg <- pkg %>%
+          add_resource("column-metadata", col_metadata)
+        
+        pkg$resources[[length(pkg$resources)]]$type <- "column-metadata"
+      }
+
+      # iso8601 date string
+      now <- strftime(as.POSIXlt(Sys.time(), "UTC"), "%Y-%m-%dT%H:%M:%S%z")
+
+      # create custom "io" metadata section
+      mdata <- self$recipe
+
+      # generate dataset uuid and add to metadata
+      mdata$data$uuid <- UUIDgenerate()
+
+      # update provenance DAG
+      new_node <- list(
+        action = action,
+        io = list(
+          "name" = "io-r",
+          "time" = now,
+          "version" = private$version
+        )
+      )
+
+      new_edge <- list(list(source=prev_pkg$io$data$uuid, target=mdata$data$uuid))
+
+      mdata$provenance$nodes[[mdata$data$uuid]] <- new_node
+      mdata$provenance$edges <- c(mdata$provenance$edges, new_edge)
 
       # add io metadata section
       pkg$io <- mdata
