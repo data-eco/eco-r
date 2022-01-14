46077b9ce954306e95c38b755d7884fe3f9e7947
Keith Hughitt <keith.hughitt@gmail.com>
2022-01-13 18:31:07 -0700

    DataDAG refactor

--- R/Packager.R
+++ R/Packager.R
@@ -1,8 +1,10 @@
-#' An R6 class for assisting with the packaging of "Io" x "DataDAG" formatted data packages
+#' An R6 class for assisting with the packaging of "Io" x "DataDAG" formatted data
+#' packages
 #'
 #' @importFrom frictionless add_resource create_package read_package
 #' @importFrom glue glue
 #' @importFrom R6 R6Class
+#' @importFrom jsonlite read_json
 #' @importFrom uuid UUIDgenerate
 #' @importFrom yaml read_yaml
 #' @name Packager
@@ -20,170 +22,255 @@ Packager <- R6Class("Packager",
     #'
     #' @return A new `Packager` object.
     initialize = function() {
-      private$conf_dir <- file.path(getwd(), "conf")
-      private$schema_dir <- file.path(getwd(), "schema")
-
-      # TODO: load data model config files
-      # self.analyses = self._load_config("analyses")
-      # self.assays = self._load_config("assays")
-      # self.platforms = self._load_config("platforms")
+      private$schema_dir <- system.file("extdata", "profiles", package = "iodat")
     },
 
     #' @description
     #' Builds Io/DataDAG data package.
     #'
-    #' @param recipe Io x DataDAG recipe
-    #' @param resources list List of resources to include in package
-    build_package = function(recipe, resources) {
-      # load & validate recipe
-      self$recipe <- private$load_recipe(recipe)
+    #' @param resources       List of resources (dataframes/tibbles) to include in package
+    #' @param annotations     Vector of filepaths to annotation files, or string annotations
+    #' @param views           List of filepaths to vega-lite views, or list representations of such views
+    #' @param node_metadata   [Optional] Additional node-level metadata to include,
+    #'                        either as a list, or path to a yml/json file; Node level metadata is *not*
+    #'                        carried over to child nodes.
+    #' @param dag_metadata    [Optional] Additional dag-level metadata to include,
+    #'                        either as a list, or path to a yml/json file; DAG-level metadata *is* copied over
+    #'                        to child packages by default; if the same field is specified downstream, the
+    #'                        value of the field is updated.
+    #' @param profile         [Optional] Name of metadata profile to use for validation, or "", for none
+    #' @param pkg_dir         [Optional] Location where data package should be saved (default: "./")
+    #' @param include_summary [Optional] Whether or not to compute & embed summary statistics at the time of package creation (default: FALSE)
+    build_package = function(resources, annotations=c(), views=c(), 
+                             node_metadata=list(),
+                             dag_metadata=list(), profile="", pkg_dir="./",
+                             include_summary=FALSE) {
+      # parse dag- and node-level metadata
+      node_mdata <- private$parse_metadata(node_metadata)
+      dag_mdata <- private$parse_metadata(dag_metadata)
 
+      # if a metadata profile was specified, validate metadata against its schema
+      # TODO (Jan22)
+      # if (profile != "") {
+      #     private$validate_metadata(node_mdata, profile)
+      # }
+
+      # determine location to build packages
+      pkg_dir <- normalizePath(pkg_dir)
+
+      # initialize data package
       pkg <- create_package()
 
+      # add resources to data package
       i <- 1
 
       for (resource_name in names(resources)) {
         pkg <- pkg %>%
           add_resource(resource_name, resources[[resource_name]])
 
-        pkg$resources[[i]]$num_rows <- nrow(resources[[resource_name]])
-        pkg$resources[[i]]$num_columns <- ncol(resources[[resource_name]])
+        # add num rows/columns to metadata;
+        # pkg$resources[[i]]$num_rows <- nrow(resources[[resource_name]])
+        # pkg$resources[[i]]$num_columns <- ncol(resources[[resource_name]])
 
         i <- 1 + 1;
       }
 
-      #pkg$resources[[1]]$type <- "dataset"
-      #pkg$resources[[length(pkg$resources)]]$type <- "row-metadata"
-      #pkg$resources[[length(pkg$resources)]]$type <- "column-metadata"
-
-      # iso8601 date string
-      now <- strftime(as.POSIXlt(Sys.time(), "UTC"), "%Y-%m-%dT%H:%M:%S%z")
-
-      # create custom "io" metadata section
-      mdata <- self$recipe
-
       # generate dataset uuid and add to metadata
-      mdata$data$uuid <- UUIDgenerate()
+      node_uuid <- UUIDgenerate()
 
-      # convert recipe provenance section to a dag
-      mdata$provenance <- list(
-        "nodes" = setNames(list(mdata$provenance), mdata$data$uuid),
-        "edges" = list()
+      node <- private$create_node(annotations, views)
+
+      node$metadata <- node_mdata
+
+      # add io-dag section to data package metadata
+      pkg$`io-dag` <- list(
+          "uuid" = node_uuid,
+          "nodes" = list(),
+          "edges" = c()
       )
+      pkg$`io-dag`$nodes[[node_uuid]] <- node
 
-      # add packager-related details
-      mdata$provenance$nodes[[mdata$data$uuid]][["software"]] <- list(
-        "name" = "io-r",
-        "time" = now,
-        "version" = private$version
-      )
+      # add dag-level metadata fields, if provided
+      pkg$`io-dag`[["metadata"]] <- dag_mdata
 
-      # add io metadata section
-      pkg$io <- mdata
+      #reserved_keys <- names(pkg$`io-dag`)
 
-      return(pkg)
+      # kwargs <- list(...)
+      #
+      # for (key in kwargs) {
+      #     if (key %in% reserved_keys) {
+      #         stop(glue("Attempting to use reserved metadata field: {key}"))
+      #     }
+      #
+      #     pkg$`io-dag`[[key]] <- kwargs[[key]]
+      # }
+
+      # write data package to disk
+      pkg %>%
+        write_package(pkg_dir)
     },
 
+
     #' @description
-    #' Builds a new Io/DataDAG data package, using an old one
+    #' Updates an existing Io datapackage.
     #'
-    #' @param path Path to original datapackage
-    #' @param metadata List with changes to be made to the metadata
-    #' @param action Name of action to use in provenance entry
-    #' @param resources list List of resources to include in data package
-    #' @param annotations list (Optional) list of markdown annotations
-    #' @param views list (Optional) list of vega-lite views to include
-    #' 
-    update_package = function(path, metadata, action, resources,
-                              annotations=list(),
-                              views=list()) {
-      # extract io block from previous datapackage, and apply any updates
-      if (!file.exists(path)) {
-        stop(glue("[Error] No datapackage found at {path}!"))
-      }
+    #' @param existing         Path to existing datapackage.json to be extended
+    #' @param resources        List of names resources to include in the data package
+    #' @param annotations      Vector of filepaths to annotation files, or string annotations
+    #' @param views            Vector of filepaths to vega-lite views, or dict representations of such views
+    #' @param node_metadata    [Optional] Additional node-level metadata to include,
+    #'                         either as a list, or path to a yml/json file; Node level metadata is *not*
+    #'                         carried over to child nodes.
+    #' @param dag_metadata     [Optional] Additional dag-level metadata to include,
+    #'                         either as a list, or path to a yml/json file; DAG-level metadata *is* copied over
+    #'                         to child packages by default; if the same field is specified downstream, the
+    #'                         value of the field is updated.
+    #' @param profile          [Optional] Name of metadata profile to use for validation, or "", for none
+    #' @param pkg_dir          [Optional] Location where data package should be saved (default: "./")
+    #' @param include_summary  [Optional] Whether or not to compute & embed summary statistics at the time of package creation (default: False)
+    update_package = function(existing, resources, annotations=c(), views=c(),
+                              node_metadata=list(), dag_metadata=list(), 
+                              profile="", pkg_dir="./",
+                              include_summary=False) {
+        # determine location to build packages
+        pkg_dir <- normalizePath(pkg_dir)
 
-      prev_pkg <- read_package(path)
+        # parse dag- and node-level metadata
+        node_mdata <- private$parse_metadata(node_metadata)
+        dag_mdata <- private$parse_metadata(dag_metadata)
 
-      self$recipe <- prev_pkg$io
-      self$recipe <- modifyList(self$recipe, metadata)
+        # add resources to data package
+        i <- 1
 
-      # create new data package and add resources
-      pkg <- create_package()
+        for (resource_name in names(resources)) {
+          pkg <- pkg %>%
+            add_resource(resource_name, resources[[resource_name]])
+          i <- 1 + 1;
+        }
 
-      i <- 1
+        node <- private$create_node(annotations, views, action = 'update_package')
 
-      for (resource_name in names(resources)) {
-        pkg <- pkg %>%
-          add_resource(resource_name, resources[[resource_name]])
+        node$metadata <- node_mdata
 
-        pkg$resources[[i]]$num_rows <- nrow(resources[[resource_name]])
-        pkg$resources[[i]]$num_columns <- ncol(resources[[resource_name]])
+        # extract io metadata + dag from existing data package
+        if (!file.exists(existing)) {
+            stop(glue("Invalid path to existing data package provided: {existing}"))
+        }
 
-        i <- 1 + 1;
-      }
+        existing_mdata <- jsonlite::read_json(existing)
 
-      # iso8601 date string
-      now <- strftime(as.POSIXlt(Sys.time(), "UTC"), "%Y-%m-%dT%H:%M:%S%z")
+        # generate dataset uuid and add to metadata
+        node_uuid <- UUIDgenerate()
 
-      # create custom "io" metadata section
-      mdata <- self$recipe
+        # copy over metadata section from previous stage
+        pkg$`io-dag` <- existing_mdata$`io-dag`
 
-      # generate dataset uuid and add to metadata
-      mdata$data$uuid <- UUIDgenerate()
+        # apply any changes to dag-level metadata
+        for (key in dag_mdata) {
+          pkg$`io-dag`$metadata[[key]] <- dag_mdata[[key]]
+        }
 
-      # update provenance DAG
-      new_node <- list(
-        action = action,
-        software = list(
-          "name" = "io-r",
-          "time" = now,
-          "version" = private$version
-        )
-      )
+        # store uuid of previous step, and update
+        prev_uuid <- pkg$`io-dag`$uuid
+        pkg$`io-dag`$uuid <- node_uuid
 
-      new_edge <- list(list(source = prev_pkg$io$data$uuid, target = mdata$data$uuid))
+        # update provenance DAG
+        pkg$`iodag`$nodes[[node_uuid]] <- node
+        pkg$`iodag`$edges <- append(pkg$`iodag`$edges, list(
+            "source" = prev_uuid,
+            "target" = node_uuid
+        ))
 
-      mdata$provenance$nodes[[mdata$data$uuid]] <- new_node
-      mdata$provenance$edges <- c(mdata$provenance$edges, new_edge)
-
-      # add annotations
-      mdata$annotations <- annotations
-
-      # add views, if present
-      for (view_name in names(views)) {
-        view <- views[[view_name]]
-
-        pkg$views[[view_name]] <- list(
-          "name" = view_name,
-          "title" = view$description,
-          "resources" = view$data,
-          "specType" = "vega-lite",
-          "spec" = view
-        )
-      }
-
-      # add io metadata section
-      pkg$io <- mdata
-
-      return(pkg)
+        # write data package to disk
+        pkg %>%
+          write_package(pkg_dir)
     }
   ),
   private = list(
     config = NULL,
     conf_dir = NULL,
     schema_dir = NULL,
-    version = "0.5.0",
+    version = "0.6.0",
 
-    load_recipe = function(path) {
-      if (!file.exists(path)) {
-        stop(glue("[Error] No recipe file found at {path}!"))
+    # @description
+    # Generates metadata block for a new node in the DAG
+    #
+    # @param annotations Vector of filepaths to annotation files, or string annotations
+    # @param views Vector of filepaths to vega-lite views, or list representations of such views
+    # @param action String to include as the "action" used to generate the node.
+    create_node = function(self, annotations, views, action='build_package') {
+        # iso8601 date string
+        now <- strftime(as.POSIXlt(Sys.time(), "UTC"), "%Y-%m-%dT%H:%M:%S%z")
+
+        node <- list(
+            "name" = "io-r",
+            "action" = action,
+            "time" =  now,
+            "version" = private$version,
+            "annot" = c(),
+            "views" = c()
+        )
+
+        # add annotations and views, if present
+        for (annot in annotations) {
+            node[["annot"]] <- append(node[["annot"]], private$parse_annotation(annot))
+        }
+
+        for (view in views) {
+            node[["views"]] <- append(node[["views"]], private$parse_view(view))
+        }
+
+        node
+    },
+
+    # @description
+    # Parses metadata and validates it against a specific profile, if specified
+    #
+    # @param mdata Path to metadata yml|json, or, a list representation
+    #
+    # @return metadata as a list
+    parse_metadata = function(mdata)  {
+      # if path specified, attempt to load mdata from file
+      if (!is.list(mdata)) {
+        if (!file.exists(mdata)) {
+          stop("Unable to find metadata at specified location!")
+        }
+
+        # load yaml
+        if (endsWith(mdata, "yml") || (endsWith(mdata, "yaml"))) {
+          mdata <- yaml::read_yaml(mdata)
+        } else if (endsWith(mdata, "json")) {
+          mdata <- jsonlite::read_json(mdata)
+        }
       }
+      mdata
+    },
 
-      recipe <- yaml::read_yaml(path)
+    # Parses a single annotation
+    #
+    # @param annot Either an annotation string, or a path to a plain-text file
+    #
+    # @return Annotation string
+    parse_annotation = function(self, annot) {
+        if (file.exists(annot)) {
+          readr::read_file(annot)
+        } else {
+          annot
+        }
+    },
 
-      # TODO: load & validate schema
-      # schema_path <- file.path(private$schema_dir, "recipe.yml")
-      return(recipe)
+    # Parses a single view
+    #
+    # @param view Either a vega-lite view loaded into a list, or a path to a
+    # json file containg such a view.
+    #
+    # @return vega-lite view as a list
+    parse_view = function(self, view) {
+        if (file.exists(view)) {
+          jsonlite::read_json(view)
+        } else {
+          view
+        }
     }
   )
 )
